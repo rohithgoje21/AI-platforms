6236914b34452e5acb1474c4f5f803771a1166c2
rohithgoje21 <rohithgoje21@gmail.com>
2024-09-11 22:46:00 +0530

    added css for card

--- src/Card.js
+++ src/Card.js
@@ -5,7 +5,7 @@ function Card({ title, description, image, link }) {
   return (
     <>
       <div className="card">
-        <img src={image} className="card-img-top" alt={`${title} logo`} />
+        <img src={image} className="card-img" alt={`${title} logo`} />
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
           <p className="card-description">{description}</p>
